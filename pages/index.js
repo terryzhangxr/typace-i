38490cfa2032a1ea65d075a818b8494ccdb9d221
che-ui <cheyida69@gmail.com>
2025-01-31 12:43:50 +0800

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-4">
+      <h1 className="text-4xl font-bold text-center text-gray-800">
+        Welcome to Typace I
+      </h1>
+    </div>
+  );
+}
