ce4a06bb1eba4fe2debceca8c86565b7201ea981
che-ui <cheyida69@gmail.com>
2025-01-31 12:55:48 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  title: 'Typace I',
+  description: 'A modern blog built with Next.js and Tailwind CSS',
+  author: 'Your Name',
+  siteUrl: 'https://yourblog.com',
+  social: {
+    twitter: 'yourtwitter',
+    github: 'yourgithub',
+  },
+  theme: {
+    darkMode: true,
+  },
+  gitalk: {
+    clientID: 'your-gitalk-client-id',
+    clientSecret: 'your-gitalk-client-secret',
+    repo: 'your-repo',
+    owner: 'your-github-username',
+    admin: ['your-github-username'],
+  },
+};
