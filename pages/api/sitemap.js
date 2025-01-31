6a93a0e144ca42188e21b3f916fbea2cda5cd1c3
che-ui <cheyida69@gmail.com>
2025-01-31 21:38:18 +0800

    Create sitemap.js

---
+++ pages/api/sitemap.js
@@ -0,0 +1,16 @@
+import { SitemapStream, streamToPromise } from 'sitemap';
+import { Readable } from 'stream';
+
+export default async (req, res) => {
+  const links = [
+    { url: '/', changefreq: 'daily', priority: 0.3 },
+    // Add other pages
+  ];
+
+  const stream = new SitemapStream({ hostname: 'https://your-blog-url.com' });
+  const xml = await streamToPromise(Readable.from(links).pipe(stream)).then((data) => data.toString());
+
+  res.setHeader('Content-Type', 'application/xml');
+  res.write(xml);
+  res.end();
+};
