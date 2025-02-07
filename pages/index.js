39b194a803fe691893a1929f748f0c55900ba6eb
che-ui <cheyida69@gmail.com>
2025-02-07 15:38:23 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -24,6 +24,9 @@ export default function Home({ allPostsData }) {
         <a href="/api/sitemap" className="inline-block">
           <img src="https://cdn.us.mrche.top/sitemap.svg" alt="Sitemap" className="block mx-auto w-8 h-8" />
         </a>
+        <p className="mt-4">
+          由MRCHE创建的<a href="https://www.mrche.top/typace" className="text-blue-600 hover:underline">Typace</a>强势驱动
+        </p>
       </footer>
     </div>
   );
