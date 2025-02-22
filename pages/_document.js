8c3d7100eb3be86e126cc50275dd57885f765a8e
Terryzhang <terry_zxr@outlook.com>
2025-02-22 15:22:00 +0800

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,16 +1,21 @@
-import Head from 'next/head';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-export default function Home() {
-  return (
-    <div>
-      {/* 动态设置标题和描述 */}
-      <Head>
-        <title>Typace - 首页</title>
-        <meta name="description" content="欢迎访问 Typace 首页，了解更多本博客主题的内容。" />
-      </Head>
-      {/* 页面内容 */}
-      <h1>欢迎来到 Typace</h1>
-      <p>这是一个基于next.js开发的博客主题，致力于分享生活和技术。</p>
-    </div>
-  );
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* 添加 favicon */}
+          <link rel="icon" href="/favicon.ico" />
+          <title>typace博客主题</title>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
