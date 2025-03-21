0a875ab2da0a7160011c495b9ca9844d77746773
Terryzhang <terry_zxr@outlook.com>
2025-03-21 19:34:00 +0800

    Update [slug].js

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { getSortedPostsData } from '../../lib/posts';
 import fs from 'fs';
-import path from 'path';
+import path from 'path'; 
 import matter from 'gray-matter';
 import { remark } from 'remark';
 import html from 'remark-html';
