f92f5ad5c4e4c7738856a111a4e0326c327fbfbe
Terryzhang <terry_zxr@outlook.com>
2025-03-22 22:46:18 +0800

    Update [slug].js

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -175,7 +175,7 @@ export default function Post({ frontmatter, contentHtml, recommendedPosts }) {
         window.Waline.init({
           el: '#waline-comment-container',
           serverURL: 'https://comment.mrzxr.top/',
-          dark: 'html.dark',
+          dark: isDarkMode ? 'auto' : false, // 自动跟随系统主题
           path: router.asPath,
           locale: { placeholder: '欢迎留言讨论...' },
         });
