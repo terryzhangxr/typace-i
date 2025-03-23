7ecb04d20a7861cfb2cb6301a46cb7e351b03d8c
Terryzhang <terry_zxr@outlook.com>
2025-03-23 17:31:25 +0800

    Update [slug].js

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -52,7 +52,7 @@ export default function Post({ frontmatter, contentHtml, recommendedPosts }) {
     style.textContent = `
       .page-container {
         opacity: 0;
-        transform: translateY(500px);
+        transform: translateY(100px);
         transition: all 0.6s cubic-bezier(0.4, 0, 0.2, 1);
       }
       .page-container.mounted {
