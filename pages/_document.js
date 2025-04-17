b285e5627837d3b7a0da8fa483d18a82cab26762
Terryzhang <terry_zxr@outlook.com>
2025-04-17 20:50:33 +0800

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,7 @@ class MyDocument extends Document {
       <Html>
         <Head>
           {/* 添加 favicon */}
-          <link rel="icon" href="/favicon.ico" />
+          <link rel="icon" href="https://ik.imagekit.io/terryzhang/%E5%B1%8F%E5%B9%95%E6%88%AA%E5%9B%BE%202025-04-17%20204625.png" />
           <title>theme typace</title>
         </Head>
         <body>
