2e763443b57a39cb7085bbecbdc46a2ac6eaab8c
Terryzhang <terry_zxr@outlook.com>
2025-04-20 12:22:44 +0800

    Update archive.js

--- pages/archive.js
+++ pages/archive.js
@@ -467,9 +467,9 @@ export default function ArchivePage({ postsByYear }) {
             />
           </a>
           <p className="mt-4 text-gray-600 dark:text-gray-400">
-            由MRCHE&terryzhang创建的
+            由Terryzhang&mrche创建的
             <a
-              href="https://www.mrche.top/typace"
+              href="https://github.com/terryzhangxr/typace-i"
               className="text-blue-600 hover:underline dark:text-blue-400"
             >
               Typace
