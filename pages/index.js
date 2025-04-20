b24a7617b105434273218a9701565ef07f160c4e
Terryzhang <terry_zxr@outlook.com>
2025-04-20 12:20:29 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1180,9 +1180,9 @@ export default function Home({ allPostsData }) {
             />
           </a>
           <p className="mt-4 text-gray-600 dark:text-gray-400">
-            由MRCHE&terryzhang创建的
+            由Terryzhang&mrche创建的
             <a
-              href="https://bgithub.xyz/terryzhangxr/typace-i"
+              href="https://github.com/terryzhangxr/typace-i"
               className="text-blue-600 hover:underline dark:text-blue-400"
             >
               Typace
