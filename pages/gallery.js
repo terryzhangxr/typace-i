50ee0047ed46dce03e7500514515632fe9b4608f
Terryzhang <terry_zxr@outlook.com>
2025-04-26 18:48:42 +0800

    Update gallery.js

--- pages/gallery.js
+++ pages/gallery.js
@@ -5,7 +5,7 @@ import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import Head from 'next/head';
 import Link from 'next/link';
-import Image from 'next/image';
+import Image from 'next/image'; 
 
 // 获取 gallery.md 文件内容
 export async function getStaticProps() {
